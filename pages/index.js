fd5431061242cb8d47644d74dd66a8a29673af80
Kavin <kv219700>
2021-06-19 13:44:30 -0300

    removed border, changed bg color

--- pages/index.js
+++ pages/index.js
@@ -17,19 +17,19 @@ export default function Home() {
             <Nav />
 
             <main>
-                <section className="mainLayout h-screen">
+                <section className="mainLayout bg-gray-100 h-screen">
                     <div className="max-w-full h-full pb-4 ml-12 mr-12 grid grid-cols-12 bg-gray-200">
-                        <div className="col-span-2 h-full bg-gray-100 border-l border-r border-b border-gray-400 text-black">
+                        <div className="col-span-2 h-full bg-gray-100 text-black">
                             <div className="courseNav container h-1/2">
                                 <Sidebar></Sidebar>
                             </div>
-                            <div className="workNav container border-t border-gray-400 h-1/2">
+                            <div className="workNav container h-1/2">
                                 <h1 className="mx-auto text-2xl font-bold text-black">
                                     User Settings/Profile
                                 </h1>
                             </div>
                         </div>
-                        <div className="mainContent overflow-auto col-span-10 h-full bg-gray-100 border-r border-b border-gray-400 flex items-center">
+                        <div className="mainContent overflow-auto col-span-10 h-full bg-gray-100 flex items-center">
                             <h1 className="mx-auto text-6xl font-bold text-black">
                                 Main Content
                             </h1>
