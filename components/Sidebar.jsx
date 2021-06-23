407113bc5c1554b24485665cd7e679e698571993
Yanlin Zhu <740228019@qq.com>
2021-06-23 23:54:26 +0800

    add highligted tab using useRouter by next.js routing (#14)
    Co-authored-by: Kavin <kv219700>

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,31 +1,41 @@
 import { Menu, Button } from 'antd';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { BookOutlined } from '@ant-design/icons';
 
 export const Sidebar = () => {
-    const [clicked, setClicked] = useState('1');
-
-    const handleClick = (e) => {
-        console.log('click ', e);
-        setClicked(e.key);
-    };
+    const [clicked, setClicked] = useState('0');
+    const router = useRouter();
+    const current = router.pathname;
+    const base = current.split('/')[1];
+    useEffect(() => {
+        switch (base) {
+            case 'CSCI2691':
+                setClicked('1');
+                break;
+            case 'CSCI3691':
+                setClicked('2');
+                break;
+            case 'CSCI4691':
+                setClicked('3');
+                break;
+        }
+    }, [base]);
 
     return (
         <>
-            <Menu onClick={handleClick} selectedKeys={clicked} mode="inline">
+            <Menu selectedKeys={clicked} mode="inline">
                 <Menu.Item key="1" icon={<BookOutlined />}>
-                    Course 1
+                    <Link href="/CSCI2691">CSCI 2691</Link>
                 </Menu.Item>
                 <Menu.Item key="2" icon={<BookOutlined />}>
-                    Course 2
+                    <Link href="/CSCI3691">CSCI 3691</Link>
                 </Menu.Item>
                 <Menu.Item key="3" icon={<BookOutlined />}>
-                    Course 3
-                </Menu.Item>
-                <Menu.Item key="4" icon={<BookOutlined />}>
-                    Course 4
+                    <Link href="/CSCI4691">CSCI 4691</Link>
                 </Menu.Item>
             </Menu>
         </>
