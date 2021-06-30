6961274bc43e30477f3e7087732636e8990bf437
Aref Abdi Poor <ar788349@dal.ca>
2021-06-30 19:32:42 +0400

    fixed spacing

--- components/CourseDisplays.jsx
+++ components/CourseDisplays.jsx
@@ -1,42 +1,26 @@
-import React from "react";
+import React from 'react';
 
 export const CourseDisplays = () => {
-
   return (
     <>
-      <div className="contentDisplay w-full my-2 shadow-lg bg-white">
-        <div className=" bg-white font-semibold p-2 flex">
-          <div className="flex-1 m-auto">
-            <div className="w-6 h-6 bg-red-500 rounded-sm"></div>
+      <div className='contentDisplay w-full my-2 shadow-lg bg-white'>
+        <div className=' bg-white font-semibold p-2 flex'>
+          <div className='flex-1 m-auto'>
+            <div className='inline-flex m-auto'>
+              <div className='w-6 h-6 bg-red-500 rounded-sm'></div>
+              <span className='m-auto px-2'> Author </span>
+            </div>
           </div>
-          <div className="flex-1 m-auto text-center">time uploaded</div>
-          <div className="flex-1 m-auto text-right">delete</div>
+          <div className='flex-1 m-auto text-right'>Title</div>
         </div>
+        <div className='shadow-sm w-full bg-gray-100 p-2 text-lg'>Random Text</div>
         <iframe
-          className="w-full"
-          src="https://docs.google.com/presentation/d/e/2PACX-1vT21R4FsbBh7Qsf_87W-mtgvGoryzD56B6cJ6IHSvwNzSi2KWVrIlJMaxEb2ozEfZ4N7zX0-Uof3OFC/embed?start=true&loop=false&delayms=3000"
-          frameborder="0"
-          allowfullscreen="true"
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-        ></iframe>
-      </div>
-
-      <div className="contentDisplay w-full my-2 bg-white">
-        <div className=" bg-white font-semibold p-2 flex">
-          <div className="flex-1 m-auto">
-            <div className="w-6 h-6 bg-red-500 rounded-sm"></div>
-          </div>
-          <div className="flex-1 m-auto text-center">time uploaded</div>
-          <div className="flex-1 m-auto text-right">delete</div>
-        </div>
-        <iframe
-          className="w-full"
-          src="https://docs.google.com/presentation/d/e/2PACX-1vT21R4FsbBh7Qsf_87W-mtgvGoryzD56B6cJ6IHSvwNzSi2KWVrIlJMaxEb2ozEfZ4N7zX0-Uof3OFC/embed?start=true&loop=false&delayms=3000"
-          frameborder="0"
-          allowfullscreen="true"
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
+          className='w-full h-screen'
+          src='https://docs.google.com/viewer?embedded=true&url=https://homepages.inf.ed.ac.uk/neilb/math.pdf'
+          frameborder='0'
+          allowfullscreen='true'
+          mozallowfullscreen='true'
+          webkitallowfullscreen='true'
         ></iframe>
       </div>
     </>
