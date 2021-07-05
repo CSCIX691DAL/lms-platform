16bc7884e2796b624445055a0291bf28e2ff0e80
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-07-05 10:27:30 -0300

    testing

---
+++ components/AdminDisplay.jsx
@@ -0,0 +1,37 @@
+import { Sidebar } from "./Sidebar";
+import ReadFirestore from "../firebase/firestore/ReadFirestore";
+import firebase from "../initFirebase"
+
+firebase();
+
+export const AdminDisplay = () => {
+  
+  return (
+    <section className="mainLayout h-screen bg-gray-100">
+      <div className="max-w-full h-full pb-4 ml-12 mr-12 grid grid-cols-12">
+        <div className="col-span-2 h-full bg-gray-100 text-black">
+          <div className="courseNav container mt-4 h-1/4">
+            <div>
+              <h1 className="mx-auto font-bold text-black border-b-2"> Courses </h1>
+            </div>
+            <div className="mt-2">
+              <Sidebar></Sidebar>
+            </div>
+          </div>
+          <div className="workNav container h-3/4">
+            <h1 className="mx-auto font-bold text-black border-b-2">
+              Modules
+            </h1>
+          </div>
+        </div>
+        <div className="mainContent overflow-auto col-span-10 h-full bg-gray-100 flex items-center">
+          <h1 className="mx-auto text-6xl font-bold text-black">
+            Hi admin
+          </h1>
+          <div >
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
