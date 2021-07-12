100d6e4e22d61c5614663d3658a2a3554fb91df4
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-07-12 16:27:10 -0300

    fixed styling and changed date format

--- components/admin/announcements/AnnouncementTable.jsx
+++ components/admin/announcements/AnnouncementTable.jsx
@@ -15,6 +15,7 @@ export const AnnouncementTable = () => {
       const data = await response.get();
       data.docs.forEach(item=>{
         setAnn([...ann,item.data()])
+        console.log(item.data().date.toDate())
       })
     }
     fetchAnn();
@@ -23,71 +24,72 @@ export const AnnouncementTable = () => {
   return (
     <div className="mt-10 shadow overflow-hidden w-full border-b border-gray-200 sm:rounded-b-lg">
       <button className="w-full h-12 px-6 text-white font-medium transition-colors duration-150 bg-blue-400 rounded-t-lg focus:shadow-outline hover:bg-blue-500">Upload</button>
-      <table className="table-fixed min-w-full divide-y divide-gray-200"></table>
-      <thead className="bg-gray-50">
-        <tr>
-          <th
-            scope="col"
-            className="w-1/8 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Course
-          </th>
-          <th
-            scope="col"
-            className="w-1/6 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Title
-          </th>
-          <th
-            scope="col"
-            className="w-1/2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Content
-          </th>                   
-          <th
-            scope="col"
-            className="w-1/4 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Date Added
-          </th>
-          <th scope="col" className="relative px-6 py-3">
-            <span className="sr-only">Delete</span>
-          </th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {console.log(...ann)}
-        {ann && ann.map((announcement) => (
-          <tr key={announcement.title}>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm font-medium text-gray-900">
-                {announcement.course}                    
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm font-medium text-gray-900">
-                {announcement.title}                    
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm font-medium text-gray-900">
-                {announcement.content}
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm font-medium text-gray-900">
-                {announcement.date}                  
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-              <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                Delete
-              </a>
-            </td>
+      <table className="table-fixed min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="w-1/8 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Course
+            </th>
+            <th
+              scope="col"
+              className="w-1/6 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Title
+            </th>
+            <th
+              scope="col"
+              className="w-1/2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Content
+            </th>                   
+            <th
+              scope="col"
+              className="w-1/8 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Date Added
+            </th>
+            <th scope="col" className="relative px-6 py-3">
+              <span className="sr-only">Delete</span>
+            </th>
           </tr>
-        ))}
-        
-      </tbody>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {console.log(...ann)}
+          {ann && ann.map((announcement) => (
+            <tr key={announcement.title}>
+              <td className="px-4 py-4 whitespace-nowrap">
+                <div className="text-sm font-medium text-gray-900">
+                  {announcement.course}                    
+                </div>
+              </td>
+              <td className="px-4 py-4 whitespace-normal">
+                <div className="text-sm font-medium text-gray-900">
+                  {announcement.title}                    
+                </div>
+              </td>
+              <td className="px-4 py-4 whitespace-normal">
+                <div className="text-sm font-medium text-gray-900">
+                  {announcement.content}
+                </div>
+              </td>
+              <td className="px-4 py-4 whitespace-normal">
+                <div className="text-sm font-medium text-gray-900">
+                  {announcement.date.toDate().toString()}                  
+                </div>
+              </td>
+              <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                  Delete
+                </a>
+              </td>
+            </tr>
+          ))}
+          
+        </tbody>
+      </table>
     </div>
   )
 }
