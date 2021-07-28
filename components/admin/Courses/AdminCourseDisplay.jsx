b065e0be81e9c2c2f961dcc54ea70d9590efcf90
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-07-28 11:05:14 -0300

    added access denied to other admin pages

--- components/admin/Courses/AdminCourseDisplay.jsx
+++ components/admin/Courses/AdminCourseDisplay.jsx
@@ -38,13 +38,13 @@ export const AdminCourseDisplay = ({ user }) => {
   } else {
     return (
       <section className="mainLayout h-screen bg-gray-100">
-        <div className="max-w-full h-full pb-4 ml-12 mr-12">          
-          <div className="mainContent overflow-auto h-full bg-gray-100 flex items-center">
-            <h1 className="mx-auto text-4xl font-bold text-black">
-              You do not have access, please return to the home page.
+        <div className="max-w-full h-full">          
+          <div className="mainContent overflow-auto h-full flex flex-col items-center">
+            <h1 className="mx-auto text-8xl font-bold text-red-600">
+              403.
             </h1>
-            <div >
-            </div>
+            <p className="font-semibold text-xl text-red-600">Access Denied, please return to home page.</p>
+            <img src="https://wallpapercave.com/wp/wp4266693.png" alt="denied" className="mx-auto" />
           </div>
         </div>
       </section>
